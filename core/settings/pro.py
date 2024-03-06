70a714fb5bccbc42c6080f485c59ac1c23b514d6
rahim <rahim.aghareb@gmail.com>
2024-03-06 11:24:47 +0330

    initial project

---
+++ core/settings/pro.py
@@ -0,0 +1,8 @@
+from .base import *
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = pro_secret_key
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
