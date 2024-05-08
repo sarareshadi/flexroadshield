3eb7fa6ec2aa647935ba5ce42c669dd865672ccf
sarareshadi <reshadi.jdu>
2024-05-08 17:50:46 +0330

    add static pages

--- generals/views.py
+++ generals/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 
-
 def HomeView(request):
     context = dict(
         page_title='Home'
@@ -10,17 +9,48 @@ def HomeView(request):
         request,
         'home.html',
         context
-
     )
 
 
 def AboutView(request):
     context = dict(
-        page_title='About us'
+        page_title='About Us'
     )
     return render(
         request,
         'about.html',
         context
-
     )
+
+
+def ContactView(request):
+    context = dict(
+        page_title='Contact Us'
+    )
+    return render(
+        request,
+        'contact.html',
+        context
+    )
+
+
+def ProjectsView(request):
+    context = dict(
+        page_title='Projects'
+    )
+    return render(
+        request,
+        'Projects.html',
+        context
+    )
+
+
+def ServicesView(request):
+    context = dict(
+        page_title='Services'
+    )
+    return render(
+        request,
+        'services.html',
+        context
+    )            
