b361e84a7a6525c3fdeabfe9ba645a9dcabacb99
sarareshadi <reshadi.jdu>
2024-05-15 19:48:14 +0330

    add pagination

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import BlogPost 
+from django.core.paginator import Paginator
 
 def BlogList(request):
-    posts = BlogPost.objects.filter(status='PB')   
+    posts_list = BlogPost.objects.filter(status='PB')   
+    paginator = Paginator(posts_list, 9)  
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
     return render(
     request, 
     'blog/bloglist.html', 
