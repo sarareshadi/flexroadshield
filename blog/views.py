6b1140e32a127c6509eb8f74c5d185194f151186
rahim <rahim.aghareb@gmail.com>
2024-05-18 18:01:59 +0330

    refactor some details in blog post list page

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import BlogPost 
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def BlogList(request):
     posts_list = BlogPost.objects.filter(status='PB')   
-    paginator = Paginator(posts_list, 9)  
+
+    # posts = paginator.get_page(page_number)
+
+    
     page_number = request.GET.get('page')
-    posts = paginator.get_page(page_number)
+    paginator = Paginator(posts_list, 9)  
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(
     request, 
     'blog/bloglist.html', 
