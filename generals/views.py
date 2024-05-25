be74785267cc36d65b6b4087487ad5fb1472597f
rahim <rahim.aghareb@gmail.com>
2024-05-25 09:18:51 +0330

    refactor home page to show blog posts

--- generals/views.py
+++ generals/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+from blog.models import BlogPost
 
 def HomeView(request):
+    posts = BlogPost.objects.filter(status=BlogPost.Status.PUBLISHED).order_by('-published_date', '-created_on')[:3]
     context = dict(
-        page_title='Home'
+        page_title='Home',
+        posts=posts
     )
     return render(
         request,
