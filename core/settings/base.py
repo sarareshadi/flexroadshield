1587f225d27b72aca470aa2ee4b7295bb0c97caa
sarareshadi <reshadi.jdu>
2024-06-02 12:39:56 +0330

    404-500 style

--- core/settings/base.py
+++ core/settings/base.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = base_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = [] 
 
 
 # Application definition
